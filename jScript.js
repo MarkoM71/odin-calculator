e89b962a8834998b2e5126a3045b2c3244d2fc6a
Marko Matura <Marko.Matura@gmail.com>
2023-01-19 16:14:52 -0500

    Create functions for basic math operators

---
+++ jScript.js
@@ -0,0 +1,31 @@
+function add(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+
+//console.log(add(7, 10));
+
+
+function subtract(num1, num2) {
+    let difference = num1 - num2;
+    return difference;
+}
+
+//console.log(subtract(10, 7));
+
+
+function multiply(num1, num2) {
+    let multiplication = num1 * num2;
+    return multiplication;
+}
+
+//console.log((multiply(10, 7)));
+
+
+function divide(num1, num2) {
+    let division = num1 / num2;
+    return division;
+}
+
+//console.log((divide(12, 6)));
+
