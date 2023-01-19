bbc16ff17a1464372b146a7eb09ae1b1338f0fa3
Marko Matura <Marko.Matura@gmail.com>
2023-01-19 16:39:30 -0500

    Create operator function

--- jScript.js
+++ jScript.js
@@ -29,3 +29,26 @@ function divide(num1, num2) {
 
 //console.log((divide(12, 6)));
 
+//Create Operate Function
+function operator(num1, num2, operator) {
+    if (operator === '+') {
+        return add(num1, num2);
+    }
+
+    else if (operator === '-') {
+        return subtract(num1, num2);
+    }
+
+    else if (operator === '*') {
+        return multiply(num1, num2);
+    }
+
+    else if (operator === '/') {
+        return divide(num1, num2);
+    }
+}
+
+console.log(operator(10, 7, '+'));
+console.log(operator(10, 7, '-'));
+console.log(operator(10, 7, '*'));
+console.log(operator(12, 6, '/'));
